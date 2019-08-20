a4098a30e3fde3b4ec085e72b497f9221235dadf
Rafael Pieri Almeida <46181942866@info.local>
2019-08-20 11:29:30 -0300

    20.08.2019

---
+++ BackEnd/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Sstop.WebApi.Domains;
+using Senai.Sstop.WebApi.Repositories;
+
+namespace Senai.Sstop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class EstilosController : ControllerBase
+    {
+        List<EstiloDomain> estilos = new List<EstiloDomain>()
+            {
+                new EstiloDomain { IdEstilo= 1, Nome = "Rock"}
+                ,new EstiloDomain { IdEstilo= 2, Nome = "Pop"}
+                ,new EstiloDomain { IdEstilo= 3, Nome = "Rap"}
+            };
+        EstiloRepository EstiloRepository = new EstiloRepository();
+    
+        [HttpGet]
+        public IEnumerable<EstiloDomain> Listar()
+        {
+            return EstiloRepository.Listar();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            EstiloDomain Estilo = EstiloRepository.BuscarPorId(id);
+            if (Estilo == null)
+            {
+                return NotFound();
+            }
+            return Ok(Estilo);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EstiloDomain estiloDomain)
+        {
+            EstiloRepository.Cadastrar(estiloDomain);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(EstiloDomain estiloDomain)
+        {
+            EstiloRepository.Alterar(estiloDomain);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            EstiloRepository.Deletar(id);
+            return Ok();
+        }
+    }
+}
---
+++ BackEnd/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,123 @@
+﻿using Senai.Sstop.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Sstop.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        List<EstiloDomain> estilos = new List<EstiloDomain>()
+            {
+                new EstiloDomain { IdEstilo= 1, Nome = "Rock"}
+                ,new EstiloDomain { IdEstilo= 2, Nome = "Pop"}
+                ,new EstiloDomain { IdEstilo= 3, Nome = "Rap"}
+            };
+
+        private string StringConexao =
+            "Data Source=.\\SqlExpress; Initial catalog=M_SStop; User Id=sa; Pwd=132";
+
+        public void Cadastrar(EstiloDomain estilo)
+        {
+            string Query = "INSERT INTO Estilos (Nome) VALUES (@Nome)";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", estilo.Nome);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public EstiloDomain BuscarPorId(int id)
+        {
+            string Query = "SELECT IdEstilo, Nome FROM Estilos WHERE IdEstilo = @IdEstilo";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                con.Open();
+                SqlDataReader sdr;
+
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+                {
+                    cmd.Parameters.AddWithValue("@IdEstilo", id);
+                    sdr = cmd.ExecuteReader();
+
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            EstiloDomain estilo = new EstiloDomain
+                            {
+                                IdEstilo = Convert.ToInt32(sdr["IsEstilo"]),
+                                Nome = sdr["Nome"].ToString()
+                            };
+                            return estilo;
+                        }
+                    }
+                    return null;
+                }
+            }
+        }
+
+        public List<EstiloDomain> Listar()
+        {
+            List<EstiloDomain> estilos = new List<EstiloDomain>();
+                using (SqlConnection con = new SqlConnection(StringConexao))
+                {
+                string Query = "SELECT IdEstilo, Nome FROM Estilos ORDER BY IdEstilo asc";
+
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+
+                    rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    EstiloDomain estilo = new EstiloDomain
+                    {
+                        IdEstilo = Convert.ToInt32(rdr["IdEstilo"]),
+                        Nome = rdr["Nome"].ToString()
+                    };
+                    estilos.Add(estilo);
+                }
+                }
+                return estilos;
+        }
+
+        public void Alterar(EstiloDomain estiloDomain)
+        {
+            string Query = "UPDATE Estilos SET Nome = @Nome WHERE IdEstilo = @IdEstilo";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", estiloDomain.Nome);
+                cmd.Parameters.AddWithValue("@IdEstilo", estiloDomain.IdEstilo);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            string Query = "DELETE FROM Estilos WHERE IdEstilo = @IdEstilo";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@IdEstilo", id);
+
+                con.Open();
+                
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
